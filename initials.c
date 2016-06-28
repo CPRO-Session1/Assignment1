b54e5d5761007a8ad870be5e8e04633931cf89c6
Harry <harrybrickner@gmail.com>
2016-06-28 18:41:13 -0400

    Did the assignment

---
+++ initials.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Harry Brickner
+ * Prints my initials using asterisks
+ */
+int main(){
+    printf("***  ***  ***** \n");
+    printf("***  ***  **  **\n");
+    printf("********  ***** \n");
+    printf("***  ***  **  **\n");
+    printf("***  ***  ***** \n");
+
+}

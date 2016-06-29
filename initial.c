4acd05389e146d8ca86dd119755bc8409329d025
Sheqi0620 <sheqi0620@icloud.com>
2016-06-29 10:12:11 -0400

    Create initial.c

---
+++ initial.c
@@ -0,0 +1,13 @@
+#include <initials.c> /*Sheqi Zhang 2016/06/29*/
+
+main()
+{
+	printf("Hello Z-world!/n")
+	printf("******/n")
+	printf("    */n")
+	printf("   */n")
+	printf("  */n")
+	printf(" */n")
+	printf("*/n")
+	printf("******/n")
+}

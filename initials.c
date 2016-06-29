0e2d531429b4f9bb373df2751fdb8297402fc6df
Olivierlu1 <180256@hkis.edu.hk>
2016-06-29 00:53:18 -0400

    added hw

---
+++ initials.c
@@ -0,0 +1,10 @@
+//Olivier Gabison - Wrote a simple piece of code depicting my initials.
+#include <stdio.h>
+
+int main() {
+printf("*******   *******");
+printf("*     *   *");
+printf("*     *   *    ***");
+printf("*     *   *     *");
+printf("*******   *******");
+}

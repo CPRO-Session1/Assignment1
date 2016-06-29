28efc7d617d9c821b8379ea1c5d433b5976cec1c
elik723 <elikschool@gmail.com>
2016-06-29 09:02:13 -0400

    Eli Kalish, Assignment 1

---
+++ EK.c
@@ -0,0 +1,10 @@
+/* Eli Kalish, Prints my Initials */
+
+#include <stdio.h>
+
+int main() {
+printf("******* *     * \n*       *    * \n*       *   * \n*       *  * \n\
+****    * * \n*       *  * \n*       *   * \n*       *    * \n\
+******* *     *");
+return 0;
+}

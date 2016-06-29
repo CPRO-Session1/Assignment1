3037692e427a37451d96012a2e2efe1e66d94110
julialaurentan <jtt2119@columbia.edu>
2016-06-29 00:45:53 -0400

    Printing my initials in asterisks

--- initials.c
+++ initials.c
@@ -1,11 +1,15 @@
+// Julia Tan
+// Initials Assignment; initials of name: JT
 #include <stdio.h>
 
 int main(){
 printf("********* *********\n");
-printf("    *         *   \n");
-printf("    *         *   \n");
-printf("    *         *   \n");
-printf("*   *         *   \n");
-printf("*****         *   \n");
+printf("********* *********\n");
+printf("    **        **   \n");
+printf("    **        **   \n");
+printf("    **        **   \n");
+printf("**  **        **   \n");
+printf("******        **   \n");
+printf("******        **   \n"); 
 return 0;
-}
+
